9fd2049fb32b9a1d61fb20d28e59c4b3e950cccc
Igor Pechenkin <ipechenkin@gmail.com>
2021-12-31 22:37:41 +0300

    implement add tasks

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,24 +1,32 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 
-export const TodoForm: React.FC = () => {
-  const [title, setTitle] = useState<string>('');
+interface TodoFormProps {
+  onAdd(title: string): void
+}
 
-  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-  };
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+  // const [title, setTitle] = useState<string>('');
+  const ref = useRef<HTMLInputElement>(null);
+
+  // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value);
+  // };
 
   const keyPressHandler = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter') {
-      console.log(title)
-      setTitle('');
+      props.onAdd(ref.current!.value);
+      ref.current!.value = '';
+      // console.log(title)
+      // setTitle('');
     }
   }
 
   return (
     <div className="input-field mt2">
       <input
-        onChange={changeHandler}
-        value={title}
+        // onChange={changeHandler}
+        // value={title}
+        ref={ref}
         type="text"
         id="title"
         placeholder="Введите название дела"
