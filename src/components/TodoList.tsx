5573f06e2b3498ae7d90ca8b58d356581c6253c3
Igor Pechenkin <ipechenkin@gmail.com>
2021-12-31 23:08:14 +0300

    add basic functional, add, toggle, delete

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {ITodo} from "../interfaces";
 
 type TodoListProps = {
-  todos: ITodo[]
+  todos: ITodo[],
+  onToggle(id: number): void,
+  onRemove: (id: number) => void
 };
 
-export const TodoList: React.FC<TodoListProps> = ({todos}) => {
+export const TodoList: React.FC<TodoListProps> = ({todos, onRemove, onToggle}) => {
   return (
     <ul>
       {todos.map(todo => {
@@ -17,9 +19,18 @@ export const TodoList: React.FC<TodoListProps> = ({todos}) => {
         return (
           <li className={classes.join(' ')} key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed} />
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={onToggle.bind(null, todo.id)}
+              />
               <span>{todo.title}</span>
-              <i className="material-icons red-text">delete</i>
+              <i
+                className="material-icons red-text"
+                onClick={() => onRemove(todo.id)}
+              >
+                delete
+              </i>
             </label>
           </li>
         );
